dcb41bec2a9b306ea8e3afa3fc8b7c65ebf1533b
golevv <golev2382@icloud.com>
2020-02-07 16:42:20 +0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '<', '>' ],
+
+        responsive:{
+            0:{
+                items:0
+            },
+
+            1000:{
+                items:1
+            }
+        }
+    });
+
+});
